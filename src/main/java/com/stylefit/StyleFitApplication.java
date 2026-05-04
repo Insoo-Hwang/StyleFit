edbccb0fb6303866d1b96286a457ceb5499a3f30
insoo.hwang <iaminsoo@naver.com>
2026-05-04 16:58:20 +0900

    first commit

---
+++ src/main/java/com/stylefit/StyleFitApplication.java
@@ -0,0 +1,13 @@
+package com.stylefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleFitApplication.class, args);
+	}
+
+}
